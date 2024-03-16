26a5d925d9d94b7a6b582c001b04e4eaf7ad4553
Aditya Dixit <112797684+AdityaManojDixit@users.noreply.github.com>
2024-03-16 09:25:36 +0000

    Conversion of datatypes

---
+++ Basics/datatype.js
@@ -0,0 +1,24 @@
+"use strict"; //Treat all JS code as newer version
+
+//alert("Hello") //We are using NodeJs not Browser
+
+let score = "33abc"
+let valueInNumber = Number(score)
+//console.log(typeof(valueInNumber))
+//console.log(valueInNumber) //Returns NaN (Not a Number)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Aditya"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false, "Aditya" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+//console.log(stringNumber)
+
---
+++ Basics/variables.js
@@ -0,0 +1,17 @@
+const account_id = 144567
+let accountEmail = "itop68@gmail.com"
+var accountPassword = "12345" //No Block & Functional Scoping. Prefer not to use.
+accountCity = "Jaipur"
+let accountState
+
+
+accountEmail = "dsdsc@gmail.com"
+accountPassword = "23233"
+
+console.log(accountEmail)
+console.log(accountPassword);
+console.table([account_id, accountEmail, accountPassword, accountCity, accountState])
+
+
+
+
